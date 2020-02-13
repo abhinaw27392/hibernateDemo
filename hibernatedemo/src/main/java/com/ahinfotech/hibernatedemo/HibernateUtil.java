22af50c8cd81bb4bce7ebb2e1260404bea502f3c
Abhinaw kumar <abhinaw27392@gmail.com>
2020-02-13 17:02:19 +0530

    changes in hibernateUtil

--- hibernatedemo/src/main/java/com/ahinfotech/hibernatedemo/HibernateUtil.java
+++ hibernatedemo/src/main/java/com/ahinfotech/hibernatedemo/HibernateUtil.java
@@ -1,6 +1,5 @@
 package com.ahinfotech.hibernatedemo;
 
-import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
@@ -8,20 +7,19 @@ import org.hibernate.service.ServiceRegistryBuilder;
 
 public class HibernateUtil {
 
-	private static final SessionFactory sessionFactory = buildSessionFactory();
+	private static SessionFactory sessionFactory;
 
 	// creating sessionFactory for session generation
-	private static SessionFactory buildSessionFactory() throws HibernateException {
-		Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Laptop.class)
-				.addAnnotatedClass(Student.class)
-				.addAnnotatedClass(Employee.class);
-
-		ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
-		SessionFactory factory = con.buildSessionFactory(reg);
-		return factory;
-	}
-
 	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Laptop.class)
+					.addAnnotatedClass(Student.class).addAnnotatedClass(Employee.class);
+
+			ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(con.getProperties())
+					.buildServiceRegistry();
+			sessionFactory = con.buildSessionFactory(reg);
+		}
+
 		return sessionFactory;
 	}
 
